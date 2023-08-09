0165ba8241b9031517da69d6e62e1ff1e63cf0c9
Samantha <samantha.yu0611@gmail.com>
2023-08-09 16:05:23 +0800

    modify: finetunePrapareData.py修正 1. 新增函式fileTransferToPreparedData，可將jsonl格式的檔案，每個prompt都如OpenAI要求加入Suffix " ->"，以及每個completion都加入Suffix "\n"。(等同執行OpenAI的CLI: openai tools fine_tunes.prepare_data -f <LOCAL_FILE>的相關要求) 2. 將函式validateJsonlFile改為uploadFile，因為此函式功能無法進行驗證，為上傳檔案到OpenAI後，取得file_id到OpenAI後，取得file_id 3. 備註，單執行此py為取得upload的檔案是否已上傳完成的狀態。
    issue: 19#issue-1842723795

--- finetunePrepareData.py
+++ finetunePrepareData.py
@@ -1,22 +1,44 @@
 import openai
 import os
+import json
 
-openai.organization = os.getenv("OPENAI_API_ORG")
+# openai.organization = os.getenv("OPENAI_API_ORG")
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-file_id = os.getenv("OPENAI_FILE_ID")
-file_path = "cleaned.jsonl"
+file_id = os.getenv("OPENAI_TRAININGFILE_ID")
+file_path = os.getenv("SOURCE_FILE")
+cleanedFileName = os.getenv("PREPARED_FILE")
 
+def fileTransferToPreparedData(): #將原始Jsonl檔案，按照openai訓練模型要求，prompt的suffix加上" ->"，completion的suffix加上 "\n"
+    with open (file_path, "r", encoding="UTF-8") as file:
+        content1 = file.readline()
+        while content1:
+            content2 = json.loads(content1.strip())
+            
+            prompt = content2['prompt']
+            completion = content2['completion']
+            
+            promptModified = prompt + ' ->'
+            completionModified = completion + '\n'
+            
+            content3 = {'prompt': promptModified, 'completion': completionModified}
+            # print(content3)
 
-def getFileStatus(file_id): #取得上傳檔案的狀態，是否已驗證完成
+            with open(cleanedFileName, "a", encoding="UTF-8") as file2:
+                json.dump(content3, file2, ensure_ascii=False)
+                file2.write('\n')
+                
+            content1 = file.readline()
+
+def getFileStatus(file_id): #取得已上傳檔案的資訊
     response = openai.File.retrieve(file_id)
     return response
 
 
-def validateJsonlFile(file_path): #將JsonL的檔案交給openai提供的工具File.create與File.retrieve做資料驗證
+def uploadFile(cleanedFileName): #將準備好的JsonL的檔案交給openai File.create上傳檔案
     
     # 上傳資料
-    response = openai.File.create(file=open(file_path, "rb"), purpose="fine-tune")
+    response = openai.File.create(file=open(cleanedFileName, "rb"), purpose="fine-tune")
     
     # 取得上傳後的資料ID
     file_id = response["id"]
@@ -26,13 +48,7 @@ def validateJsonlFile(file_path): #將JsonL的檔案交給openai提供的工具F
     response = openai.File.retrieve(file_id)
     print(response["status"])
     
-    # 取得已驗證的資料內容
-    # response = openai.File.retrieve(file_id)
-    # print(response["file"]["filename"])
-    # print(response["file"]["url"])
-    
     return file_id
-
 
 
 if __name__ == "__main__":
