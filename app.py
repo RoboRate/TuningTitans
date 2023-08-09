1dc8b13fd7f551a6c294eae63754379f90cd9e9a
a2265535 <ko29469501@gmail.com>
2023-08-10 00:03:50 +0800

    modify:"Complete data download functionality."

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 from flask import Flask, redirect, render_template, request, session, url_for, send_file
 import openai
 import pymysql
-
+import shutil
+import json
+from models import createJsonlFromDocuments
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -37,22 +39,31 @@ def index():
 
     # 現在可以在這裡處理表單提交，因為使用者已經登入
     if request.method == "POST":
-        file = request.files["fileToUpload"]
-        if file:
-            filename = file.filename
+        files = request.files["fileToUpload"]
+        if files:
+            filename = files.filename
             if filename.endswith(".txt") or filename.endswith(".csv"):
-                content = file.read().decode("utf-8")
+                content = files.read().decode("utf-8")
                 previewData = content  # 將檔案內容傳遞到前端供預覽
-
+                # Create a uploads_directory to store the uploaded files
+                uploads_directory = './uploads'
+                os.makedirs(uploads_directory, exist_ok=True)
                 # 儲存上傳的檔案至伺服器端，以便下載
                 filePath = os.path.join("uploads", filename)
-                file.save(filePath)
-
-                # 儲存內容至檔案
-                with open(os.path.join("uploads", filename), "w", encoding="utf-8",newline=os.linesep) as f:
-                    f.write(content)
-
+                files.save(filePath)
+                try:
+                # Generate the JSONL data from the uploaded documents轉檔JSONL神器
+                    result = createJsonlFromDocuments(filePath)
+                # Write the JSONL data to the output file
+                    output_file = './uploads/output.jsonl'  # 暫存
+                    with open(output_file, 'w', encoding='utf-8') as file:
+                        for item in result:
+                            file.write(json.dumps(item, ensure_ascii=False) + '\n')
+                    return send_file(output_file, as_attachment=True)
+                finally:
+                # Remove the uploads_directory after processing
+                    shutil.rmtree(uploads_directory)
     return render_template("index.html", previewData=previewData, filePath=filePath)
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
