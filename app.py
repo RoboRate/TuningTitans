f3212560d36a4efb44d5c39666ba08e40a94f149
a2265535 <ko29469501@gmail.com>
2023-08-17 15:17:01 +0800

    fix:"修正與前端互動變數位置"
    新增刪除暫存資料夾
    刪除冗行
    
    issus #36

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ import finetuneModelCreate as createModel
 import finetunePrepareData as prepareData
 import Benchmark as benchmarkreport
 from werkzeug.utils import secure_filename
-
+import tempfile
+import atexit
+import shutil
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -35,10 +37,6 @@ def logout():
     session.pop("logged_in", None)
     return redirect(url_for("login"))
 
-@app.before_request
-def before_request():
-    g.temp_dirs_to_delete = [] 
-
 @app.route("/qadesign", methods=["GET", "POST"])
 def index():
     if not session.get("logged_in"):
@@ -46,7 +44,8 @@ def index():
 
     previewData = None  # 初始化檔案預覽內容為空
     filePath = None  # 初始化檔案路徑為空
-
+    #刪除暫存資料夾
+    atexit.register(cleanup)
     # 現在可以在這裡處理表單提交，因為使用者已經登入
     if request.method == "POST":
         files = request.files["fileToUpload"]
@@ -54,7 +53,7 @@ def index():
             filename = files.filename
             if filename.endswith(".txt") or filename.endswith(".csv"):
                 content = files.read().decode("utf-8")
-                previewData = content  # 將檔案內容傳遞到前端供預覽
+                #previewData = content  # 將檔案內容傳遞到前端供預覽
                 
                 # Create an uploads_directory to store the uploaded files
                 uploads_directory = './uploads'
@@ -62,26 +61,33 @@ def index():
                 
                 # 儲存上傳的檔案至伺服器端，以便下載
                 temp_dir = tempfile.mkdtemp(dir=uploads_directory)
-                temp_file_path = os.path.join(temp_dir, filename)
+                filePath = os.path.join(temp_dir, filename)
                 
-                with open(temp_file_path, 'w', encoding='utf-8') as file:
+                with open(filePath, 'w', encoding='utf-8') as file:
                     file.write(content)  # 將內容寫入文件
                     
-                try:
-                    # Generate the JSONL data from the uploaded documents轉檔JSONL神器
-                    result = createJsonlFromDocuments(temp_dir)  # 提供目錄路徑
-                    output_file = os.path.join(uploads_directory, 'output.jsonl')
-                    
-                    with open(output_file, 'w', encoding='utf-8') as file:
-                        for item in result:
-                            file.write(json.dumps(item, ensure_ascii=False) + '\n')
-                    return send_file(output_file, as_attachment=True)
-                finally:
-                    # Remove the temporary directory after processing
-                    shutil.rmtree(temp_dir)
                 
+                    # Generate the JSONL data from the uploaded documents轉檔JSONL神器
+                result = createJsonlFromDocuments(temp_dir)  # 提供目錄路徑
+                output_file = os.path.join(uploads_directory, 'output.jsonl')
+                    
+                with open(output_file, 'w', encoding='utf-8') as file:
+                    for item in result:
+                        file.write(json.dumps(item, ensure_ascii=False) + '\n')
+                    #return send_file(output_file, as_attachment=True)
+                previewData = result
+                atexit.register(cleanup)
     return render_template("index.html", previewData=previewData, filePath=filePath)
+uploads=None
+def cleanup():
+    if uploads:
+        try:
+            shutil.rmtree(uploads)  # 直接刪除整個文件夾及其內容
+        except Exception as e:
+            print(f"Error while cleaning up: {e}")
 
+# 清理房子
+atexit.register(cleanup)
 @app.route("/finetuning", methods=["GET", "POST"])
 def finetuning():
     if not session.get("logged_in"):
